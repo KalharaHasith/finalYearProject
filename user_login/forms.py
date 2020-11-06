aa83a43d05d8aced92bd0a72d2b6094e62297c96
Hasith Kalhara <hasithkalhara@gmail.com>
2020-11-07 01:13:50 +0530

    add loginpage

---
+++ user_login/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from user_login.models import UserProfileInfo
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+
+
